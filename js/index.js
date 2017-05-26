e51338a44930baaf60a647af49f3335129a53171
ashpotak <alexandr.shpotak@gmail.com>
2017-05-26 22:52:01 +0300

    v.0.3
    - deleted fullpage scrolling
    - added side indicators of scrolling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $('#fullpage').fullpage();
-});
+(function ($) {
+  $(function () {
+    $('.scroll-section').scrollIndicatorBullets({
+      scrollOffset: 0,
+      titleSelector: 'h1,h2,h3',
+      pgKeysEnabled: true,
+      scrollDuration: 1000
+    });
+  });
+})(jQuery);
