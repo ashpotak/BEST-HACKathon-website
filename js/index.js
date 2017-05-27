be5a9b4286a4d36597e425823da91a519d677571
ashpotak <alexandr.shpotak@gmail.com>
2017-05-27 16:20:07 +0300

    v.0.5
    - Preloader

--- js/index.js
+++ js/index.js
@@ -34,3 +34,21 @@
         window.scroll({ top: 0, left: 0, behavior: 'smooth' });
       });
     });
+
+ //hellopreloader
+ var hellopreloader = document.getElementById("hellopreloader_preload");
+ function fadeOutnojquery(el){
+   el.style.opacity = 1;
+   var interhellopreloader = setInterval(function(){
+     el.style.opacity = el.style.opacity - 0.05;
+     if (el.style.opacity <=0.05){
+        clearInterval(interhellopreloader);
+        hellopreloader.style.display = "none";
+     }
+   },16);
+   }
+   window.onload = function(){
+     setTimeout(function(){
+       fadeOutnojquery(hellopreloader);
+   },1000);
+ };
