7738cd70826f34314a8858eff04112773272e0eb
ashpotak <alexandr.shpotak@gmail.com>
2017-05-27 15:08:40 +0300

    v.0.4
    - Back button

--- js/index.js
+++ js/index.js
@@ -8,3 +8,29 @@
     });
   });
 })(jQuery);
+
+
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scrollToTop').fadeIn();
+        } else {
+            $('#scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    //$('.scrollToTop').click(function(){
+    //    $('html, body').animate({scrollTop : 0},800);
+    //    return false;
+    //});
+
+// add event listener on load
+    window.addEventListener('load', function() {
+      // scroll to top
+      document.querySelector('.js-scroll-to-top').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+      });
+    });
