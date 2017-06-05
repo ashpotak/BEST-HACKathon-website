1001fef7be95c2741570242f80cc23a206fe999a
ashpotak <alexandr.shpotak@gmail.com>
2017-06-05 22:23:42 +0300

    v.0.9
    - timer

--- js/index.js
+++ js/index.js
@@ -184,5 +184,5 @@
     var timeinterval = setInterval(updateClock, 1000);
   }
 
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  var deadline = new Date(Date.parse(new Date()) + 87 * 24 * 60 * 60 * 1000);
   initializeClock('clockdiv', deadline);
