f3218e98e8899cb57e75be585525f9c7795ad3fa
ashpotak <alexandr.shpotak@gmail.com>
2017-06-05 20:57:48 +0300

    v.0.8.9
    - "do you know"-block
    - pre-timer

--- js/index.js
+++ js/index.js
@@ -116,6 +116,18 @@
       menu.classList.toggle( "active" );
     });
 
+    document.querySelector( "#yes" )
+      .addEventListener( "click", function() {
+        ifyes.classList.toggle( "active" );
+        question.classList.toggle( "active" );
+      });
+
+    document.querySelector( "#no" )
+        .addEventListener( "click", function() {
+          ifno.classList.toggle( "active" );
+          question.classList.toggle( "active" );
+        });
+
 
     $(document).ready(function(){
   	$("#body").on("click","a", function (event) {
@@ -132,3 +144,45 @@
   		$('body,html').animate({scrollTop: top}, 1500);
   	});
   });
+
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+
+  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  initializeClock('clockdiv', deadline);
